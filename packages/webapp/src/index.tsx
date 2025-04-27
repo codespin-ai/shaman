82294a0ef491c04b6f0cc7887d95c7a232b4031d
Jeswin Kumar <jeswinpk@agilehead.com>
2025-04-27 09:58:45 +0530

    Initial commit

---
+++ packages/webapp/src/index.tsx
@@ -0,0 +1,14 @@
+// Import all components for registration
+import "./MainLayout.js";
+import "./Header.js";
+import "./Footer.js";
+import "./HomePage.js";
+import "./CategoryPage.js";
+import "./ListingDetail.js";
+import "./NotFoundPage.js";
+import "./Sidebar.js";
+import "./CategorySelectionPage.js";
+import "./LocationSelectionPage.js";
+import "./auth/LoginPage.js";
+import "./auth/ProfilePage.js";
+import "./auth/UserProfile.js";
